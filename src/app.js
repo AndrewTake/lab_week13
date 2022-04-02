361e59927c4dbc10e78ca86be66e5f804c017211
AndrewTake <andrew6041@gmail.com>
2022-04-01 19:45:44 -0700

    part 2 delete created a clear copy button using a function. The function copies the code and replaces it with ' ', reseting the text.

--- src/app.js
+++ src/app.js
@@ -1,20 +1,19 @@
 // step one, define the variables that will be used in the function
-const originalContainer = document.querySelector('.original-container')
-const copyContainer = document.querySelector('.copy-container')
+const originalContainer = document.querySelector('.original-container');
+const copyContainer = document.querySelector('.copy-container');
 // step two, build the function
-originalContainer.addEventListener('click'), (event) => {
-    if (event.target = div.original-container) {
-        copyContainer.insertAdjacentHTML("beforebegin", event.target.innerHTML)
-
-
-
-        // insertAdjacentHTML(position, text), represents the position
-        // relative to the element
+originalContainer.addEventListener('click', (event) => {
+    if (event.target = 'div.original-container') {
+        copyContainer.insertAdjacentHTML("beforeend", event.target.innerHTML)
+        // insertAdjacentHTML(position, text), represents the position relative to the element
+        // innerHTML sets the HTML markup within an element, in thihs case, its being copied
     }
 }
+)
+;
+const clearTheCopies = document.querySelector('button');
 
-const btn = document.querySelector("button")
-
-btn.addEventListener("click", () => {
-    cloneArea.innerHTML = ''
-})
+clearTheCopies.addEventListener('click', (event) => {
+    copyContainer.innerHTML = ''
+}
+)
