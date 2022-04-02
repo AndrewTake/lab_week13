c545649acdd0c7021143b9b4d00ebb7e2c75f136
AndrewTake <andrew6041@gmail.com>
2022-04-01 20:40:53 -0700

    fixed an issue where one and two would both appear when the middle of the two buttons was clicked. Now only one appears when one is clicked: same goes for two

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 // step one, define the variables that will be used in the function
 const originalContainer = document.querySelector('.original-container');
 const copyContainer = document.querySelector('.copy-container');
+const containerOne = Array.from(document.querySelectorAll('div'))
+    .find(element => element.textContent === 'one')
+
 // step two, build the function
 originalContainer.addEventListener('click', (event) => {
     if (event.target = 'div.original-container') {
-        copyContainer.insertAdjacentHTML("beforeend", event.target.innerHTML)
+        copyContainer.insertAdjacentHTML("beforeend", event.target.innerHTML + ' ')
         // insertAdjacentHTML(position, text), represents the position relative to the element
-        // innerHTML sets the HTML markup within an element, in thihs case, its being copied
+        // innerHTML sets the HTML markup within an element, in this case, its being copied
     }
 }
 )
